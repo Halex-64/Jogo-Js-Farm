1669256677c10a940a2a2c8e273ba20eafe9f451
Laura <laurachristidasilva@gmail.com>
2023-09-19 11:08:21 -0300

    Primeiro commit jogo

---
+++ JS/main.js
@@ -0,0 +1,22 @@
+const personagem = document.getElementById('personagem');
+let posicaoHorizontal = 0;
+let posicaoVertical = 0;
+const step = 10;
+
+function updatePersonagemPosition() {
+    personagem.style.left = posicaoHorizontal + 'px';
+    personagem.style.top = posicaoVertical + 'px';
+  }
+  
+  document.addEventListener('keydown', (event) => {
+    switch (event.key) {
+      case 'ArrowLeft':
+        posicaoHorizontal -= step;
+        break;
+      case 'ArrowRight':
+        posicaoHorizontal += step;
+        break;
+    }
+  
+    updatePersonagemPosition();
+  });
